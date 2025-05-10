370bdcb27a9ad3e8fdbb9fecf0a993ba62f7b22a
Gfmonteiro04 <gf.monteiro04@gmail.com>
2025-05-10 16:35:24 -0300

    subindo auth

---
+++ app/auth/__init__.py
@@ -0,0 +1,3 @@
+# C:\Users\ianes\Desktop\AS Cloud\app\auth\__init__.py
+
+from .routes import auth_bp
---
+++ app/auth/forms.py
@@ -0,0 +1,17 @@
+# C:\Users\ianes\Desktop\AS Cloud\app\auth\forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired(), Length(3, 80)])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(6, 128)])
+    confirm = PasswordField('Confirme a senha',
+                            validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrar')
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired(), Length(3, 80)])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(6, 128)])
+    submit = SubmitField('Entrar')
